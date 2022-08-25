1458d11666601bb5c31969a5ae7a07741c490f1e
watarichanno <watarichanno@gmail.com>
2022-08-25 20:47:27 +0700

    Add playing with many rounds and scorekeeping

--- game.js
+++ game.js
@@ -17,26 +17,61 @@ function getPlayerChoice() {
 
 function calcResult(playerChoice, computerChoice) {
     if (playerChoice === computerChoice)
-        return "A draw!";
+        return "draw";
 
     if (computerChoice == "rock") {
         if (playerChoice == "paper")
-            return "You won! Rock is beaten by Paper";
+            return "won";
         if (playerChoice == "scissor")
-            return "You lose! Rock beats Scissor";
+            return "lose";
     }
 
     if (computerChoice == "paper") {
         if (playerChoice == "rock")
-            return "You lose! Paper beats Rock";
+            return "lose";
         if (playerChoice == "scissor")
-            return "You won! Paper is beaten by Scissor";
+            return "won";
     }
 
     if (computerChoice == "scissor") {
         if (playerChoice == "rock")
-            return "You won! Scissor is beaten by Rock";
+            return "won";
         if (playerChoice == "paper")
-            return "You lose! Scissor beats Paper";
+            return "lose";
     }
+}
+
+function capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+
+        let result = calcResult(playerChoice, computerChoice);
+
+        if (result === "draw") {
+            console.log(`Round ${i + 1}: A draw!`);
+        } else if (result === "won") {
+            console.log(`Round ${i + 1}: You won! ${capitalize(computerChoice)} is beaten by ${capitalize(playerChoice)}`);
+            playerScore++;
+        } else {
+            console.log(`Round ${i + 1}: You lose! ${capitalize(computerChoice)} beats ${capitalize(playerChoice)}`);
+            computerScore++;
+        }
+    }
+
+    if (computerScore > playerScore) {
+        console.log("You lost to the computer!");
+    } else if (computerScore < playerScore) {
+        console.log("You won! Congratulation!");
+    } else {
+        console.log("This is a draw!");
+    }
+    console.log(`Final score: You = ${playerScore} Computer = ${computerScore}`);
 }
